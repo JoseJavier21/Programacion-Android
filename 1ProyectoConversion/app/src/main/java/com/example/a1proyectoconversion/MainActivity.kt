083f258b5786a4cf63e30dea0d20c1e9611c3050
JoseJavier21 <javier.rojo@escuelaestech.es>
2022-02-18 10:48:18 +0100

    Inicios
    añadido de imagenes y creacion de codigo

--- 1ProyectoConversion/app/src/main/java/com/example/a1proyectoconversion/MainActivity.kt
+++ 1ProyectoConversion/app/src/main/java/com/example/a1proyectoconversion/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a1proyectoconversion
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -8,25 +9,24 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import com.example.a1proyectoconversion.databinding.ActivityMainBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val divisa1 = findViewById<EditText>(R.id.primeramoneda)
-        val divisa2 = findViewById<EditText>(R.id.segundamoneda)
-        val spinner1 = findViewById<Spinner>(R.id.spinner1)
-        val spinner2 = findViewById<Spinner>(R.id.spinner2)
-        val convertidor = findViewById<Button>(R.id.convertir)
-        val resultado = findViewById<TextView>(R.id.resultado)
-        val compartir = findViewById<FloatingActionButton>(R.id.compartir)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
+    //      Boton para compartir      //
 
 
-        compartir.setOnClickListener {
+        binding.compartir.setOnClickListener {
 
             try {
                 val uri = Uri.parse("twitter://user?screen_name=escuelaestech")
@@ -38,6 +38,24 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+
+
+    //      Primer eleccion de moneda        //
+
+        val monedaoro = 1
+        val aro = 5
+        binding.spinner1.setOnClickListener {
+
+            val cambio = monedaoro + aro
+
+        }
+
+
+
+
+    //      Cambiar icono de moneda     //
+
+
 
 
     }
