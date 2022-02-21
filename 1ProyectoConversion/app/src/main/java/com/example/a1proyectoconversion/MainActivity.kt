ca72c45850a1df06c68dc91113fed5084bfccbba
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2022-02-21 22:10:00 +0100

    añadido gif
    se le ha añadido el gif y corregido unos pequeños problemas, no tiene codigo
    terminar

--- 1ProyectoConversion/app/src/main/java/com/example/a1proyectoconversion/MainActivity.kt
+++ 1ProyectoConversion/app/src/main/java/com/example/a1proyectoconversion/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.a1proyectoconversion
 
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.a1proyectoconversion.databinding.ActivityMainBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,21 +34,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-    //      Primer eleccion de moneda        //
-
-        val monedaoro = 1
-        val rupia = 5
-        val credito = 2
-        val gil = 4
-        val aro = 6
-        val banara =  3
-        binding.spinner1.setOnClickListener {
-
-
-        }
-
-
-    //      Cambiar icono de moneda     //
+    //  Convertidor de monedas  //
 
 
 
