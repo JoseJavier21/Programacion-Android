60aa02092a2c81d27ff1bb9f43c8d0259ee59728
JoseJavier21 <javier.rojo@escuelaestech.es>
2022-03-03 10:22:41 +0100

    Boton de cancelar

---
+++ RecyclerViewExample/app/src/main/java/com/estech/recyclerviewexample/Myadaptador.kt
@@ -0,0 +1,46 @@
+package com.estech.recyclerviewexample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.estech.recyclerviewexample.databinding.HolderPlayerBinding
+
+class Myadaptador(val lista: MutableList<String>): RecyclerView.Adapter<Myadaptador.PlayerHolder>() {
+
+    inner class PlayerHolder (view: View): RecyclerView.ViewHolder(view){
+        val textView: TextView
+
+        init {
+            textView = view.findViewById(R.id.holder_name)
+
+
+        }
+    }
+
+
+    inner class Jugadoresbinding(binding:HolderPlayerBinding): RecyclerView.ViewHolder(binding.root){
+
+        val layoutManager: LinearLayoutManager(this)
+                binding.recycler.view
+    }
+
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerHolder {
+//        val layoutInflater = LayoutInflater.from(parent.context)
+//        val view = layoutInflater.inflate(R.layout.holder_player, parent, false)
+//        val playerHolder = PlayerHolder(view)
+//        return playerHolder
+//    }
+
+    override fun onBindViewHolder(holder: PlayerHolder, position: Int) {
+        val jugador = lista[position]
+        holder.textView.text = jugador
+
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+}
