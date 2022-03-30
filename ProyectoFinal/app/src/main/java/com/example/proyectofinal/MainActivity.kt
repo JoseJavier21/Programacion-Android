3606b99798d6fd1a1c137ff17ce2c09d73042632
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2022-03-30 18:37:48 +0200

    avance

--- ProyectoFinal/app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ ProyectoFinal/app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.proyectofinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.proyectofinal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +14,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val usuario = binding.usuario
+        val contrasenia = binding.contrasenia
 
+
+        binding.acceder.setOnClickListener {
+
+            if(usuario.editableText.isEmpty() && contrasenia.editableText.isEmpty()){
+                usuario.setError("Campo vacio")
+                contrasenia.setError("Campo vacio")
+
+            }else if(usuario){
+
+            }else{}
+
+
+        }
 
 
     }
-
-
 }
